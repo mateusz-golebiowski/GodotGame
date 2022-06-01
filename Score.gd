71b5429a0078792583813ac2ad4efb22b92c347c
Mateusz Gołębiowski <golebiowski.mateusz@outlook.com>
2022-06-01 20:32:37 +0200

    explosions

--- Score.gd
+++ Score.gd
@@ -8,9 +8,9 @@ extends RichTextLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.bbcode_text = "[center]Game over\nYour score: " + String(Global.score) + "[/center]"
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	self.text = "Score: " + String(Global.score)
